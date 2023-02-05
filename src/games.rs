44752e8cefb8dd3f134babb0a63cc50dbf51a0f0
Matthew Merrill <git@mattmerr.com>
2023-02-05 14:21:51 -0800

    start implementing gameinfo webhooks

--- src/games.rs
+++ src/games.rs
@@ -1,34 +1,32 @@
-static DEFAULT_NEWCULAR: &str = "      - N R K R N -
-- - - B - - -
-- - - B - - -
-P - P - P - P
-- - - - - - -
-p - p - p - p
-- - - b - - -
-- - - b - - -
-- n r k r n -";
+use std::collections::HashMap;
 
-pub enum GameType {
-    Newcular,
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct GameType {
+    pub name: String,
+    pub webhook: String,
 }
 
+#[derive(Serialize)]
+pub struct GameSummary {
+    pub valid_moves: Vec<String>,
+    pub render: String,
+    pub winner: Option<i8>,
+}
+
+static registry_string : &str = "[{\"name\":\"newcular\",\"webhook\":\"http://localhost:8181/gameType/newcular/summary/\"}";
+
+static registry: HashMap<String, GameType> =
+    serde_json::from_str::<Vec<GameType>>(registry_string)
+        .unwrap().iter().map(|&game|(game.name, game)).collect();
+
 impl GameType {
-    pub fn to_string(&self) -> String {
-        match self {
-            Newcular => "newcular".to_string(),
-        }
+    pub fn from_string(game_type: &str) -> Option<&GameType> {
+        registry.get(game_type)
     }
 
-    pub fn from_string(game_type: &str) -> Option<Self> {
-        match game_type {
-            "newcular" => Some(GameType::Newcular),
-            _ => None,
-        }
-    }
-
-    pub fn default_render(&self) -> &str {
-        match self {
-            Newcular => DEFAULT_NEWCULAR,
-        }
+    pub fn summary(&self, moves: Vec<String>) -> GameSummary {
+        
     }
 }
